59d6718a1675e37a4d91d818f3b2ab45418b1d7b
Seong-Min Na <seongmin.na@snu.ac.kr>
2020-01-20 20:15:34 +0900

    2019.1

---
+++ memory_test/Debug/src/memorytest.d
@@ -0,0 +1,35 @@
+src/memorytest.o src/memorytest.o: ../src/memorytest.c \
+ ../../memory_test_bsp/microblaze_0/include/xparameters.h \
+ ../../memory_test_bsp/microblaze_0/include/xil_types.h \
+ ../../memory_test_bsp/microblaze_0/include/xstatus.h \
+ ../../memory_test_bsp/microblaze_0/include/xil_types.h \
+ ../../memory_test_bsp/microblaze_0/include/xil_assert.h \
+ ../../memory_test_bsp/microblaze_0/include/xil_testmem.h \
+ ../src/platform.h ../src/platform_config.h ../src/memory_config.h \
+ ../../memory_test_bsp/microblaze_0/include/xil_printf.h \
+ ../../memory_test_bsp/microblaze_0/include/xparameters.h \
+ ../../memory_test_bsp/microblaze_0/include/bspconfig.h
+
+../../memory_test_bsp/microblaze_0/include/xparameters.h:
+
+../../memory_test_bsp/microblaze_0/include/xil_types.h:
+
+../../memory_test_bsp/microblaze_0/include/xstatus.h:
+
+../../memory_test_bsp/microblaze_0/include/xil_types.h:
+
+../../memory_test_bsp/microblaze_0/include/xil_assert.h:
+
+../../memory_test_bsp/microblaze_0/include/xil_testmem.h:
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../src/memory_config.h:
+
+../../memory_test_bsp/microblaze_0/include/xil_printf.h:
+
+../../memory_test_bsp/microblaze_0/include/xparameters.h:
+
+../../memory_test_bsp/microblaze_0/include/bspconfig.h:
---
+++ memory_test/Debug/src/platform.d
@@ -0,0 +1,25 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../memory_test_bsp/microblaze_0/include/xparameters.h \
+ ../../memory_test_bsp/microblaze_0/include/xil_cache.h \
+ ../../memory_test_bsp/microblaze_0/include/mb_interface.h \
+ ../../memory_test_bsp/microblaze_0/include/xil_types.h \
+ ../../memory_test_bsp/microblaze_0/include/xil_assert.h \
+ ../../memory_test_bsp/microblaze_0/include/xil_exception.h \
+ ../../memory_test_bsp/microblaze_0/include/xparameters.h \
+ ../src/platform_config.h
+
+../../memory_test_bsp/microblaze_0/include/xparameters.h:
+
+../../memory_test_bsp/microblaze_0/include/xil_cache.h:
+
+../../memory_test_bsp/microblaze_0/include/mb_interface.h:
+
+../../memory_test_bsp/microblaze_0/include/xil_types.h:
+
+../../memory_test_bsp/microblaze_0/include/xil_assert.h:
+
+../../memory_test_bsp/microblaze_0/include/xil_exception.h:
+
+../../memory_test_bsp/microblaze_0/include/xparameters.h:
+
+../src/platform_config.h:
